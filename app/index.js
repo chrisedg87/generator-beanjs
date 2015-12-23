bd30beb46766ed20492f9ae23c64f5b150959f62
chrisedg87 <chrisedg87@me.com>
2015-12-23 16:31:02 +0000

    Added DB Config to app config

--- app/index.js
+++ app/index.js
@@ -116,7 +116,7 @@ module.exports = generators.Base.extend({
 
     log.green('Cloning the BEAN repo.......');
 
-    exec('git clone --branch ' + versions[version] + ' https://github.com/beanjs/bean.git ' + folder)
+    exec('git clone --branch ' + versions[version] + ' https://github.com/chrisedg87/BeanJS.git ' + folder)
       .then(function () {
         done();
       })
@@ -145,6 +145,11 @@ module.exports = generators.Base.extend({
       name: 'addArticleExample',
       message: 'Would you like to generate the article example CRUD module?',
       default: true
+    }, {
+      type: 'confirm',
+      name: 'addDevDatabase',
+      message: 'Would you like to generate the development database connection config?',
+      default: true
     }];
 
     this.prompt(prompts, function(props) {
@@ -153,7 +158,7 @@ module.exports = generators.Base.extend({
       this.appKeywords = props.appKeywords;
       this.appAuthor = props.appAuthor;
       this.addArticleExample = props.addArticleExample;
-      this.addChatExample = props.addChatExample;
+      this.addDevDatabase = props.addDevDatabase
 
       this.slugifiedAppName = s(this.appName).slugify().value();
       this.humanizedAppName = s(this.appName).humanize().value();
@@ -161,6 +166,62 @@ module.exports = generators.Base.extend({
 
       done();
     }.bind(this));
+  },
+
+  promptForDatabase: function() {
+    var done = this.async();
+
+    if(this.addDevDatabase){
+      var prompts = [{
+        name: 'dbHost',
+        message: 'What is your Database server host address?',
+        default: 'db_host'
+      }, {
+        name: 'dbUser',
+        message: 'What is your Database username?',
+        default: 'beanjs'
+      }, {
+        name: 'dbPass',
+        message: 'What is your Database password?',
+        default: 'somethingsecure'
+      }, {
+        name: 'dbName',
+        message: 'What is your database name?',
+        default: 'beanjs'
+      }];
+
+      this.prompt(prompts, function(props) {
+        this.dbHost = props.dbHost;
+        this.dbUser = props.dbUser;
+        this.dbPass = props.dbPass;
+        this.dbName = props.dbName;
+        done();
+      }.bind(this));
+    }
+  },
+
+  applyDbConfig: function() {
+    var done = this.async();
+    if(this.addDevDatabase){
+      var hostRegex = "db_host";
+      var userRegex = "db_user";
+      var passRegex = "db_pass";
+      var nameRegex = "db_name";
+
+      var configFile = (fs.readFileSync(folderPath + 'app/config/env/development.js')).toString();
+
+      configFile = configFile.replace(hostRegex, this.dbHost);
+      configFile = configFile.replace(userRegex, this.dbUser);
+      configFile = configFile.replace(passRegex, this.dbPass);
+      configFile = configFile.replace(nameRegex, this.dbName);
+
+      fs.writeFile(folderPath + 'app/config/env/development.js', configFile, function (err) {
+        if(err) {
+          return log.red(err);
+        }
+        done();
+      });
+    }
   },
 
   replacePackageConfigs: function () {
@@ -197,15 +258,15 @@ module.exports = generators.Base.extend({
   replaceConfigConfigs: function () {
     var done = this.async();
 
-    var titleRegex = /title: 'BEAN.JS'/g;
-    var descriptionRegex = /description: 'Node MySQL Framework'/g;
+    var titleRegex = /title: \'BeanJS\'/g;
+    var descriptionRegex = /description: \'Node MySQL Framework\'/g;
 
-    var configFile = (fs.readFileSync(folderPath + 'config/env/default.js')).toString();
+    var configFile = (fs.readFileSync(folderPath + 'app/config/env/all.js')).toString();
 
-    configFile.replace(titleRegex, 'title: \'' + this.slugifiedAppName + '\'');
-    configFile.replace(descriptionRegex, 'description: \'' + this.appDescription + '\'');
+    configFile = configFile.replace(titleRegex, 'title: \'' + this.slugifiedAppName + '\'');
+    configFile = configFile.replace(descriptionRegex, 'description: \'' + this.appDescription + '\'');
 
-    fs.writeFile(folderPath + 'config/env/default.js', configFile, function (err) {
+    fs.writeFile(folderPath + 'app/config/env/all.js', configFile, function (err) {
       if(err) {
         return log.red(err);
       }
